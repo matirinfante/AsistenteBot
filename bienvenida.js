b93b2e08ff27fc989e00df737d5aca70afd1cf13
Matias <maty.infante@gmail.com>
2020-11-05 18:42:03 -0300

    deploying changes

--- bienvenida.js
+++ bienvenida.js
@@ -1,10 +1,10 @@
 module.exports = (client) => {
-    const channelId = '733424609814118450' // welcome channel
-    const reglasId = '642189238716203017' // reglas
-    const solicitudClaseId = '693970431748603965' // solicitud de clase
-    const solicitudNombreId = '' // solicitud de nombre
-    const solicitudHorarioId = '' // solicitud de horario
-    const rrssId = '' // redes sociales
+    const channelId = '757954702003667005' // welcome channel
+    const reglasId = '774003534738882581' // reglas
+    const solicitudClaseId = '701921320950300672' // solicitud de clase
+    const solicitudNombreId = '701921115358101504' // solicitud de nombre
+    const solicitudHorarioId = '717771774997299301' // solicitud de horario
+    const rrssId = '742132567796482050' // redes sociales
 
     client.on('guildMemberAdd', (member) => {
         console.log(member)
@@ -12,8 +12,15 @@ module.exports = (client) => {
             member.id
         }> al Batallón de la muerte, seré tu asistente de bienvenida. Si quieres ver cómo nos manejamos dentro del server pásate por ${member.guild.channels.cache
             .get(reglasId)
-            .toString()}. Para comenzar tu camino como Clone Trooper pasate por los canales ${member.guild.channels.cache
+            .toString()}. 
+            Para comenzar tu camino como Clone Trooper pasate por los canales ${member.guild.channels.cache
             .get(solicitudClaseId)
+            .toString()} ${member.guild.channels.cache
+            .get(solicitudNombreId)
+            .toString()} ${member.guild.channels.cache
+            .get(solicitudHorarioId)
+            .toString()} ${member.guild.channels.cache
+            .get(rrssId)
             .toString()}`
 
         const channel = member.guild.channels.cache.get(channelId)
